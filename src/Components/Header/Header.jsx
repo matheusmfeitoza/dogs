80b4daf22f535a734e892e2940b172466293d48a
Matheus Marinho <feitozamatheus123@gmail.com>
2022-04-05 11:25:44 -0300

    Created and stylized header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Index.module.css";
+import { ReactComponent as Dogs } from "../../Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/login">Login / Criar conta</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          {" "}
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar conta
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
