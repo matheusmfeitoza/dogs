5a07b5766333150bd440dd6e4d6c9201da908425
Matheus Marinho <feitozamatheus123@gmail.com>
2022-04-05 11:04:55 -0300

    Initial structure created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Api from "./api/Api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div>
-      <h1>
-        <Api />
-      </h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
