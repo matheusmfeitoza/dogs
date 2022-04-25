9aa953e364f978e396298a9beb3b45aadeae685a
Matheus Marinho <feitozamatheus123@gmail.com>
2022-04-25 15:04:59 -0300

    Changed style in Login form and added new animation

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -4,6 +4,7 @@ import Input from "../Form/Input/Input";
 import Button from "../Form/Button/Button";
 import useForm from "../../Hooks/useForm";
 import { UserContext } from "../../Context/UserContext";
+import Error from "../Ui/Error/Error";
 
 const LoginForm = () => {
   const username = useForm();
@@ -19,7 +20,8 @@ const LoginForm = () => {
   };
 
   return (
-    <div>
+    <section className="animaLeft">
+      <h1 className="title">Login</h1>
       <form onSubmit={handleSubmit}>
         <Input
           type="text"
@@ -35,8 +37,9 @@ const LoginForm = () => {
           <Button>Entrar</Button>
         )}
       </form>
+      <Error error={context.erro} />
       <Link to="/login/createuser">Cadastrar Usuário</Link>
-    </div>
+    </section>
   );
 };
 
