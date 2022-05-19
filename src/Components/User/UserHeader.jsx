01ccaf419e5c9d0fe855f8df6ae8e962758d1b08
Matheus Marinho <feitozamatheus123@gmail.com>
2022-05-19 16:54:37 -0300

    Styled UserHead and make a logic to change title with route

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import UserHeaderNav from "./UserHeaderNav";
+import styles from "./Styles/UserHeader.module.css";
+import { useLocation } from "react-router-dom";
 
 function UserHeader() {
+  const [titulo, setTitulo] = React.useState("");
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    switch (pathname) {
+      case "/conta/estatisticas":
+        setTitulo("Estatisticas");
+        break;
+      case "/conta/postar":
+        setTitulo("Poste sua foto");
+        break;
+      default:
+        setTitulo("Minha conta");
+    }
+  }, [pathname]);
+
   return (
-    <div>
-      <h1 className="title">Título</h1>
+    <div className={styles.header}>
+      <h1 className="title">{titulo}</h1>
       <UserHeaderNav />
     </div>
   );
