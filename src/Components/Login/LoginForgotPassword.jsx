970b10b8d3ce351e70ac822931404c1f68e9563c
Matheus Marinho Feitoza <feitozamatheus123@gmail.com>
2022-07-28 11:03:03 -0300

    Created a LostPassword Component

--- src/Components/Login/LoginForgotPassword.jsx
+++ src/Components/Login/LoginForgotPassword.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Input from "../Form/Input/Input";
+import Button from "../Form/Button/Button";
+import Error from "../Ui/Error/Error";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch.jsx";
+import { PASSWORD_lOST } from "../../api";
 
 const LoginForgotPassword = () => {
-  return <div>LoginForgotPassword</div>;
+  const login = useForm();
+  const { data, erro, loading, request } = useFetch();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_lOST({
+        login: login.value,
+        email: window.location.href.replace("forgotuser", "recoverpass"),
+      });
+      await request(url, options);
+    }
+  };
+  return (
+    <section>
+      <h1 className="title">Recupere seu acesso</h1>
+      {data ? (
+        <p style={{ color: "#1d1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            name="login"
+            placeholder="Informe seu usuário ou e-mail"
+            {...login}
+          />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar</Button>
+          )}
+        </form>
+      )}
+
+      {erro && <Error error={erro} />}
+    </section>
+  );
 };
 
 export default LoginForgotPassword;
