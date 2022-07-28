4b9a98c358fc2baf3c0a302d1fc1738215bc7544
Matheus Marinho Feitoza <feitozamatheus123@gmail.com>
2022-07-28 12:04:39 -0300

    feat: created a component reset pass

---
+++ src/Components/Login/LoginRecoverPass.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Input from "../Form/Input/Input";
+import Button from "../Form/Button/Button";
+import Error from "../Ui/Error/Error";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch.jsx";
+import { PASSWORD_RESET } from "../../api";
+import { useNavigate } from "react-router-dom";
+
+const LoginRecoverPass = () => {
+  const [key, setKey] = React.useState("");
+  const [login, setLogin] = React.useState("");
+  const { erro, loading, request } = useFetch();
+  const navigate = useNavigate();
+  const newPassword = useForm();
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const keyUrl = params.get("key");
+    const loginUrl = params.get("login");
+    if (keyUrl) setKey(keyUrl);
+    if (loginUrl) setLogin(loginUrl);
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (newPassword.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login: login,
+        password: newPassword.value,
+        key: key,
+      });
+      const { response } = await request(url, options);
+      if (response.ok) navigate("/login");
+    }
+  };
+  return (
+    <section>
+      <h1 className="title"> Recupere seu acesso</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Insira sua nova senha"
+          type="password"
+          name="password"
+          {...newPassword}
+        />
+        {loading ? (
+          <Button disabled>Redefinindo...</Button>
+        ) : (
+          <Button>Redefinir senha</Button>
+        )}
+      </form>
+      {erro && <Error error={erro} />}
+    </section>
+  );
+};
+
+export default LoginRecoverPass;
