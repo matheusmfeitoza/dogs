38ff0c4c278183731ce97d0cb5bb3f019ebd0621
Matheus Marinho Feitoza <feitozamatheus123@gmail.com>
2022-07-28 16:23:43 -0300

    feat: Created a Statics Component

--- src/Components/User/UserStatics.jsx
+++ src/Components/User/UserStatics.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Loading from "../Ui/Loading/Loading";
+import Error from "../Ui/Error/Error";
+import useFecth from "../../Hooks/useFetch";
+import UserStatisGraph from "./UserStatisGraph";
+import { STATS_GET } from "../../api";
 
 const UserStatics = () => {
-  return <div>UserStatics</div>;
+  const { data, erro, loading, request } = useFecth();
+
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const { url, options } = STATS_GET(token);
+    async function getStatisData() {
+      await request(url, options);
+    }
+    getStatisData();
+  }, [request]);
+  if (erro) return <Error error={erro} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section>
+        <UserStatisGraph data={data} />
+      </section>
+    );
+  else return null;
 };
 
 export default UserStatics;
---
+++ src/Components/User/UserStatisGraph.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { VictoryPie, VictoryBar, VictoryChart } from "victory";
+import styles from "./Styles/UserStatisGraph.module.css";
+
+const UserStatisGraph = ({ data }) => {
+  const [acessos, setAcessos] = React.useState(0);
+  const [graph, setGraph] = React.useState([]);
+
+  React.useEffect(() => {
+    const graphValues = data.map((item) => {
+      return {
+        x: item.title,
+        y: Number(item.acessos),
+      };
+    });
+    setAcessos(
+      data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b)
+    );
+    setGraph(graphValues);
+  }, [data]);
+  return (
+    <section className={`${styles.graph} animaLeft`}>
+      <div className={`${styles.acessos} ${styles.graphItem}`}>
+        Acessos: {acessos}
+      </div>
+      <div className={`${styles.graphic} ${styles.graphItem} `}>
+        <VictoryPie
+          data={graph}
+          colorScale={["tomato", "orange", "gold", "cyan", "red", "blue"]}
+          animate={{ duration: 2000 }}
+          innerRadius={40}
+          padding={{ top: 20, bottom: 20, left: 50, right: 50 }}
+        />
+      </div>
+      <div className={`${styles.graphic} ${styles.graphItem}`}>
+        <VictoryChart>
+          <VictoryBar data={graph} alignment={"start"} />
+        </VictoryChart>
+      </div>
+    </section>
+  );
+};
+
+export default UserStatisGraph;
